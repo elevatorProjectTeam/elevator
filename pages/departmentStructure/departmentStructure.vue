6cd4e302a676545f097066033c2a93e95b9a4d3b
Lidan-x <2362869094@qq.com>
2020-05-10 11:16:02 +0800

    组织架构+选择检测员

--- pages/departmentStructure/departmentStructure.vue
+++ pages/departmentStructure/departmentStructure.vue
@@ -1,26 +1,65 @@
 <template>
-	<view class="list">
-		<!-- 一级 -->
-		<scroll-view class="list-left" scroll-y :style="'height:'+height+'px'">
-			<view v-if="dataList.length>0" class="nav" @click="categoryClickMain(item.groupId,index)" :key="item.groupId" :class="index==categoryActive?'active':''" v-for="(item,index) in dataList">
-				<view>{{item.groupName}}</view>
+	<view>
+		<view class="top">
+			<view class="top-left">请选择检测员</view>
+			<view class="top-right">
+				<block v-for="(item,index) in rightView" :key="item.id" v-if="item.checked">
+					<view class="right-select">
+						<view class="select-name">{{item.name}}</view>
+						<view class="select-close"><image src="../../static/images/icon/close.png"></image></view>
+					</view>
+				</block>
+				
+				<!--
+				<view class="right-select">
+					<view class="select-name">张一</view>
+					<view class="select-close"><image src="../../static/images/icon/close.png"></image></view>
+				</view>
+				<view class="right-select">
+					<view class="select-name">张一</view>
+					<view class="select-close"><image src="../../static/images/icon/close.png"></image></view>
+				</view>
+				<view class="right-select">
+					<view class="select-name">张一</view>
+					<view class="select-close"><image src="../../static/images/icon/close.png"></image></view>
+				</view>
+				<view class="right-select">
+					<view class="select-name">张一</view>
+					<view class="select-close"><image src="../../static/images/icon/close.png"></image></view>
+				</view>
+				<view class="right-select">
+					<view class="select-name">张一</view>
+					<view class="select-close"><image src="../../static/images/icon/close.png"></image></view>
+				</view>
+				-->
 			</view>
-		</scroll-view>
-		<!-- 二级 -->
-		<scroll-view class="list-right" scroll-y :scroll-top="scrollTop" @scroll="scroll" :style="'height:'+height+'px'" scroll-with-animation>
-			<view class="content">
-			   <block v-for="(item,index) in rightView" :key="item.id">
-					<view class="row">
-						<image :src="item.src" class="row-portrait"></image>
-						{{item.name}}
-						<view class="row-icon" v-for="n in item.star">
-							<image src="../../static/images/icon/star.png"></image>
-						</view>							
-						<view v-if="item.num!=0" class="row-total">{{item.num}}</view>
-					</view>	   
-			   </block>
-			</view>
-		</scroll-view>
+		</view>
+		<view class="list">
+			<!-- 一级 -->
+			<scroll-view class="list-left" scroll-y :style="'height:'+height+'px'">
+				<view v-if="dataList.length>0" class="nav" @click="categoryClickMain(item.groupId,index)" :key="item.groupId" :class="index==categoryActive?'active':''" v-for="(item,index) in dataList">
+					<view>{{item.groupName}}</view>
+				</view>
+			</scroll-view>
+			<!-- 二级 -->
+			<scroll-view class="list-right" scroll-y :scroll-top="scrollTop" @scroll="scroll" :style="'height:'+height+'px'" scroll-with-animation>
+				<view class="content">
+				   <block v-for="(item,index) in rightView" :key="item.id">
+						<view class="row">
+							<image :src="item.src" class="row-portrait"></image>
+							{{item.name}}
+							<view class="row-icon" v-for="n in item.star">
+								<image src="../../static/images/icon/star.png"></image>
+							</view>							
+							<view v-if="item.num!=0" class="row-total">{{item.num}}</view>
+							<view v-if="item.num==0">
+								<checkbox :value="item.name" :checked="item.checked" class="row-checkbox" @click="checkBox($event, item)" ></checkbox>
+							</view>
+						</view>	   
+				   </block>
+				</view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
@@ -38,56 +77,64 @@
 								name:"总数",
 								src:"../../static/images/icon/inspector.png",
 								num:1111,
-								star:4
+								star:4,
+								checked:false
 							},
 							{
 								id:1,
 								name:"张一",
 								src:"../../static/logo.png",
 								num:0,
-								star:3
+								star:3,
+								checked:false
 							},
 							{
 								id:2,
 								name:"张一",
 								src:"../../static/logo.png",
 								num:0,
-								star:4
+								star:4,
+								checked:false
 							},
 							{
 								id:3,
 								name:"张一",
 								src:"../../static/logo.png",
 								num:0,
-								star:3
+								star:3,
+								checked:false
 							},
 							{
 								id:4,
 								name:"张一",
 								src:"../../static/logo.png",
 								num:0,
-								star:4
+								star:4,
+								checked:false
 							},
 							{
 								id:5,
 								name:"张一",
 								src:"../../static/logo.png",
 								num:0,
-								star:5
+								star:5,
+								checked:false
 							},
 							{
 								id:6,
 								name:"张一",
 								src:"../../static/logo.png",
 								num:0,
-								star:4
+								star:4,
+								checked:false
 							},
 							{
 								id:7,
 								name:"张一",
 								src:"../../static/logo.png",
 								num:0,
-								star:3
+								star:3,
+								checked:false
 							}
 						],
 					},
@@ -100,21 +147,24 @@
 								name:"总数",
 								src:"../../static/images/icon/inspector.png",
 								num:2222,
-								star:5
+								star:5,
+								checked:false
 							},
 							{
 								id:1,
 								name:"张二",
 								src:"../../static/logo.png",
 								num:0,
-								star:4
+								star:4,
+								checked:false
 							},
 							{
 								id:2,
 								name:"张二",
 								src:"../../static/logo.png",
 								num:0,
-								star:3
+								star:3,
+								checked:false
 							}
 						],
 					},
@@ -127,19 +177,22 @@
 								name:"总数",
 								src:"../../static/images/icon/inspector.png",
 								num:3333,
-								star:4
+								star:4,
+								checked:false
 							},
 							{   id:1,
 								name:"张三",
 								src:"../../static/logo.png",
 								num:0,
-								star:3
+								star:3,
+								checked:false
 							},
 							{   id:2,
 								name:"张三",
 								src:"../../static/logo.png",
 								num:0,
-								star:5
+								star:5,
+								checked:false
 							}
 						],
 					},
@@ -152,21 +205,24 @@
 								name:"总数",
 								src:"../../static/images/icon/inspector.png",
 								num:4444,
-								star:5
+								star:5,
+								checked:false
 							},
 							{
 								id:1,
 								name:"张四",
 								src:"../../static/logo.png",
 								num:0,
-								star:4
+								star:4,
+								checked:false
 							},
 							{
 								id:2,
 								name:"张四",
 								src:"../../static/logo.png",
 								num:0,
-								star:5
+								star:5,
+								checked:false
 							}
 						],
 					},
@@ -179,21 +235,24 @@
 								name:"总数",
 								src:"../../static/images/icon/inspector.png",
 								num:5555,
-								star:3
+								star:3,
+								checked:false
 							},
 							{
 								id:1,
 								name:"张五",
 								src:"../../static/logo.png",
 								num:0,
-								star:4
+								star:4,
+								checked:false
 							},
 							{
 								id:2,
 								name:"张五",
 								src:"../../static/logo.png",
 								num:0,
-								star:2
+								star:2,
+								checked:false
 							}
 						],
 					},
@@ -206,21 +265,24 @@
 								name:"总数",
 								src:"../../static/images/icon/inspector.png",
 								num:6666,
-								star:4
+								star:4,
+								checked:false
 							},
 							{
 								id:1,
 								name:"张六",
 								src:"../../static/logo.png",
 								num:0,
-								star:5
+								star:5,
+								checked:false
 							},
 							{
 								id:2,
 								name:"张六",
 								src:"../../static/logo.png",
 								num:0,
-								star:2
+								star:2,
+								checked:false
 							}
 						],
 					}	
@@ -250,7 +312,11 @@
 					console.log("this.rightView",this.rightView);
 					//console.log("current.subs",current.subs);
 				}
-			}	
+			},
+			checkBox(e, item) {
+				let that = this;
+				let box = (item.checked = !item.checked);
+			},
 		}
 	}
 </script>
@@ -259,6 +325,45 @@
 page{
 	background-color: #F2F2F2;
 	font-size:28rpx;
+}
+.top{
+	display: flex;
+	flex-direction: row;
+}
+.top-left{
+	font-weight: bold;
+	margin: 20rpx;
+}
+.top-right{
+	width: 500rpx;
+	height: 150rpx;
+	background-color: #FFFFFF;
+	margin: 20rpx;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+}
+.right-select{
+	margin: 10rpx;
+	width: 120rpx;
+	height: 50rpx;
+	background-color: #F2F2F2;
+	display: flex;
+	flex-direction: row;
+	position: relative;
+}
+.select-name{
+	margin-left: 10rpx;
+	margin-right: 15rpx;
+}
+.select-close{
+	position: absolute;
+	top: 10rpx;
+	right: 10rpx;
+}
+.select-close image{
+	width: 30rpx;
+	height: 30rpx;
 }
 .list{
 	display: flex;
@@ -318,6 +423,11 @@ page{
 	position: absolute;
 	top:40rpx;
 	right:80rpx ;
+}
+.row-checkbox{
+	position: absolute;
+	top:40rpx;
+	right:110rpx ;
 }
  
 
